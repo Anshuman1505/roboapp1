e089a4a34683175b55e5db14844bfc98935442a7
Anshuman1505 <67578515+Anshuman1505@users.noreply.github.com>
2020-07-07 19:54:47 +0530

    Updates

---
+++ precache-manifest.62834ccd26778649f45659016b33d176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9549ab92fca62fe58d5b9fbd4ce9a7",
+    "url": "/Anshuman1505/roboapp1/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Anshuman1505/roboapp1/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "32ceb487b476cafc1f31",
+    "url": "/Anshuman1505/roboapp1/static/css/main.3f11f270.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Anshuman1505/roboapp1/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Anshuman1505/roboapp1/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ceb487b476cafc1f31",
+    "url": "/Anshuman1505/roboapp1/static/js/main.33691081.chunk.js"
+  },
+  {
+    "revision": "a620d25b4b2202fa7a56",
+    "url": "/Anshuman1505/roboapp1/static/js/runtime-main.102ddc40.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Anshuman1505/roboapp1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
